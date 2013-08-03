0368dcd7a0f474eaabdd41fda95e267a6d247d34
Pavel Gatilov <pavel.gatilov@gmail.com>
2013-08-03 09:52:27 -0700

    Initial Commit

---
+++ Student Study/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Student Study
+//
+//  Created by Pavel Gatilov on 8/3/13.
+//  Copyright (c) 2013 Pavel Gatilov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Student Study/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Student Study
+//
+//  Created by Pavel Gatilov on 8/3/13.
+//  Copyright (c) 2013 Pavel Gatilov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
